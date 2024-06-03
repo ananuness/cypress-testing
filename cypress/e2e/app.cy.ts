bd2fe469aea5a31b386dde3a7bf62fefbac4d1d3
ananuness <beatriznunez601@gmail.com>
2024-06-02 22:23:47 -0300

    refactor: applying best practices

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -16,31 +16,24 @@ const colors = {
   success: 'rgb(0, 200, 0)',
 };
 
-describe('Submitting an image with invalid inputs', () => {
-  after(() => {
+describe('Form submission', () => {
+  beforeEach(() => {
     cy.clearAllLocalStorage();
   });
-  it('should be on the image registration page', () => {
+
+  it('should give errors when submitting empty inputs', () => {
     cy.visit('/');
-  });
 
-  it('should click the submit button', () => {
     registerForm.submitButton().click();
-  });
 
-  it('should show "Please type a title for the image" message above the title field', () => {
     registerForm
       .titleFeedback()
       .should('contain.text', 'Please type a title for the image');
-  });
 
-  it('should show "Please type a valid URL" message above the imageUrl field', () => {
     registerForm
       .imageUrlFeedback()
       .should('contain.text', 'Please type a valid URL');
-  });
 
-  it('should see an exclamation icon in the title and imageUrl fields', () => {
     registerForm.titleInput().should(({ 0: element }) => {
       const style = window.getComputedStyle(element);
       const border = style.getPropertyValue('border-color');
